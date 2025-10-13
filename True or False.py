3884d6c2375eaaf68b47dfb83b6a2f083eb27c55
FaramarzMohmand <mfaramarz316@gmail.com>
2025-10-13 15:41:46 +0330

    Add files via upload

---
+++ True or False.py
@@ -0,0 +1,27 @@
+print("سلام دنیا!")
+
+midterm=77
+final=23
+
+semester_grade= midterm + final
+print("Sum of midterm and final exam", semester_grade)
+
+
+num1=15
+num2=8
+sum_result = num1 + num2
+print("the sum of ",num1,"and",num2, "is", sum_result)
+
+
+
+
+
+
+# (temp>30) or presence 
+# The output should be false 
+
+
+temp= 25 
+presence= False
+result= (temp>30) or presence
+print(result) 
